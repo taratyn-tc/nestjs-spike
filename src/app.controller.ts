2fcaf309079040f89c3c543c7a9b361a9ca6cca2
Tom Aratyn <taratyn@wavelo.com>
2025-01-09 19:16:48 +0200

    Basic swagger API based documentation.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
 const EMPTY_STRING = '';
 
+@ApiTags('greeter')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
