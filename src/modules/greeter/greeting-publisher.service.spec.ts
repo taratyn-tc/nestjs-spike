2670008cefd0c4cbbbc4a88e1ca05236dec39be7
Tom Aratyn <taratyn@wavelo.com>
2025-01-17 17:39:19 +0200

    Publisher test module doesn't immediately fail.

--- src/modules/greeter/greeting-publisher.service.spec.ts
+++ src/modules/greeter/greeting-publisher.service.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GreetingPublisherService } from './greeting-publisher.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Greeted } from './greeted.entity';
 
 describe('GreetingPublisherService', () => {
   let service: GreetingPublisherService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [Greeted],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Greeted]),
+      ],
       providers: [GreetingPublisherService],
     }).compile();
 
